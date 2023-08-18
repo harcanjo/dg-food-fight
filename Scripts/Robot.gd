23561551df8e900df62c3ce5876d611309e7f71e
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-08-18 10:40:01 -0300

    game state victory as enemy count < 1

--- Scripts/Robot.gd
+++ Scripts/Robot.gd
@@ -1,8 +1,12 @@
 extends "res://Scripts/Character.gd"
 
+signal remove_enemy
+
 func _ready():
 	$RobotArmature/AnimationPlayer.get_animation("Robot_Running").set_loop(true)
 	character_type = CHARACTER_TYPES.npc
+	var gamestate = get_parente().get_parent()
+	connect("remove_enemy", gamestate, "remove_enemy")
 
 func _physics_process(delta):
 	if $RayCast.is_colliding() and can_fire:
@@ -14,3 +18,17 @@ func update_lives():
 	if lives > 0:
 		var life = $Lives.get_child(0).get_child(0)
 		life.play("lose_life")
+		
+func die():
+	emit_signal("remove_enemy")
+	queue_free()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
